0dbba92a09aad7ebee32137e2dbaec83582ba16e
Jose Figueroa <Josefigueroalv@protonmail.com>
2019-11-02 13:03:48 -0700

    the 3D box is animated, & we can drag the 3D face

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -13,7 +13,8 @@ import {
   Viro3DObject,
   ViroAmbientLight,
   ViroSpotLight,
-  ViroNode
+  ViroNode,
+  ViroAnimations
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -34,7 +35,7 @@ export default class HelloWorldSceneAR extends Component {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} />
+        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}}/>
         <ViroAmbientLight color={"#aaaaaa"} />
         <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
           position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
@@ -76,6 +77,15 @@ var styles = StyleSheet.create({
 ViroMaterials.createMaterials({
   grid: {
     diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+ViroAnimations.registerAnimations({
+  rotate: {
+    properties: {
+      rotateY: "+=90"
+    },
+    duration: 250, //.25 seconds
   },
 });
 
